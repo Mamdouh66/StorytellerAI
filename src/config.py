372c6f2453314d44672b6dea512721a4b9d05092
Mamdouh Khalaf <2210002016@iau.edu.sa>
2023-12-07 17:01:11 +0300

    ✅ Add WriterGPT class

--- src/config.py
+++ src/config.py
@@ -8,6 +8,28 @@ class Settings(BaseSettings):
     # MODEL_NAME: str = "gpt-4-1106-preview"
     MODEL_NAME: str = "gpt-3.5-turbo"
 
+    STORY_TITLE: str = "Sagas of Mythranor"
+    STORY_DESCRIPTION: str = """
+    In the enchanting world of Mythranor, a realm steeped in ancient magic and inhabited by diverse and mythical beings,
+    an epic narrative begins to unfold. This high fantasy world, comprised of four grand empires, has known an age of peace 
+    and harmony, overseen by the Celestial Sages, powerful beings who are custodians of the world's equilibrium and guardians 
+    of the mystical elements.
+    Darkness rises in the East as a once-banished warlock, known as Zarethor, re-emerges with the lost art of Dark Ether, 
+    threatening to disrupt the world's harmony. His emergence marks the beginning of an era of strife, propelling Mythranor
+    towards the brink of destruction.
+    At the heart of this saga is a young human heroine, Seraphina, a warrior with a destiny entwined with the future of Mythranor.
+    Propelled by an ancient prophecy, she embarks on a quest to unite the diverse inhabitants of Mythranor - the Skyborne Elves,
+    the subterranean Forge Dwarves, the mystical Merfolk of the Azure Depths, and the arcane Phoenix Touched.
+    Joining Seraphina is a motley crew of brave souls: Eolan, an Elf archer with a secret burden; Brom, a jovial Dwarf with
+    unmatched prowess in battle; Lysandra, a Merfolk sorceress whose magic is as unpredictable as the seas; and Orion, a Phoenix 
+    Touched with the power to harness the flames of rebirth.
+    Their mission is to recover the Celestial Crystals, ancient and powerful relics of the Celestial Sages, scattered across the 
+    furthest reaches of Mythranor, each protected by formidable trials that challenge their bonds and beliefs.
+    In their journey, they traverse through mythical landscapes, encounter legendary beasts like the shadowy Wraith Dragons and
+    the noble Griffins of the Sunspire Mountains. They face inner demons and external foes, all the while forging a bond that
+    transcends their differences.
+    """
+
     class Config:
         env_file = ".env"
 
--- src/storyteller.py
+++ src/storyteller.py
@@ -2,3 +2,82 @@ import instructor
 
 from openai import OpenAI
 from config import settings
+
+
+class WriterGPT:
+    def __init__(self):
+        self.client = OpenAI(api_key=settings.API_KEY)
+
+    def generate_story(
+        self,
+        critique: str | None,
+        character: str | None,
+        world_building: str | None,
+        previous_story: str | None,
+        critique_turn: bool = False,
+    ) -> str:
+        response = self.client.chat.completions.create(
+            model=settings.MODEL_NAME,
+            messages=[
+                self.prompt_template(
+                    self,
+                    critique=critique,
+                    character=character,
+                    world_building=world_building,
+                    previous_story=previous_story,
+                    critique_turn=critique_turn,
+                )
+            ],
+            temperature=0.3,
+        )
+        return response.choices[0].message.content
+
+    def prompt_template(
+        self,
+        critique: str,
+        character: str,
+        world_building: str,
+        previous_story: str,
+        critique_turn: bool = False,
+    ) -> str:
+        if critique_turn:
+            after_critquie_prompt = f"""
+            Based on the critique and suggestions provided, revise and enhance the high fantasy story.
+            Focus on addressing the identified areas of improvement in narrative structure, character development,
+            and world-building. Incorporate the suggested changes to enhance plot coherence, deepen character arcs,
+            and enrich the descriptive elements of the fantasy world. Ensure the story maintains a balanced flow,
+            with improved dialogue, action, and descriptive passages. Pay particular attention to refining the emotional depth 
+            and thematic resonance of the narrative. Work on crafting a more engaging and immersive storytelling experience that
+            captivates the reader from beginning to end.
+
+            critique: {critique}
+
+            character: {character}
+
+            world-building: {world_building}
+
+            keep in mind this is your previous writing: {previous_story}
+            """
+            return after_critquie_prompt
+
+        init_story_prompt = f"""
+        Write a compelling high fantasy story based with the title {settings.STORY_TITLE} and with the following description
+        {settings.STORY_DESCRIPTION} focus on the initial plot and character outlines provided.
+        Focus on creating a multi-layered narrative with detailed descriptions, complex characters, and an engaging plot structure.
+        Ensure the story unfolds with a captivating introduction, develops tension and conflict in the middle, and concludes
+        with a satisfying climax. Pay attention to crafting vivid scenes that bring the fantasy world to life, highlighting
+        its unique elements, landscapes, and creatures. write 4000 tokens.
+        """
+        return init_story_prompt
+
+
+class CritiqueGPT:
+    ...
+
+
+class CharacterGPT:
+    ...
+
+
+class WorldGPT:
+    ...
